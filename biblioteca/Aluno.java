916d27039da6279bc464d96cc8e57df78cd9c695
mkailanyss19-ai <mkailanyss19@gmail.com>
2026-08-29 21:18:17 -0300

    Update Aluno.java

--- biblioteca/Aluno.java
+++ biblioteca/Aluno.java
@@ -1,1 +1,36 @@
 Aluno.java
+
+  package biblioteca;
+
+public class Aluno extends Usuario {
+    private String curso;
+    private String turma;
+
+    public Aluno(String nome, String cpf, String email, String curso, String turma) {
+        super(nome, cpf, email);
+        this.curso = curso;
+        this.turma = turma;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getTurma() {
+        return turma;
+    }
+
+    public void setTurma(String turma) {
+        this.turma = turma;
+    }
+
+    @Override
+    public void listarInformacoes() {
+        System.out.println("[ALUNO] Nome: " + getNome() + " | CPF: " + getCpf() + 
+                           " | Email: " + getEmail() + " | Curso: " + curso + " | Turma: " + turma);
+    }
+}
