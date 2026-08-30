e19baa996126090cafbf5f24dab48901254d9e50
mkailanyss19-ai <mkailanyss19@gmail.com>
2026-08-29 21:18:46 -0300

    Update Fucionario.java

--- biblioteca/Fucionario.java
+++ biblioteca/Fucionario.java
@@ -1,1 +1,26 @@
 Fucionario.java
+
+  package biblioteca;
+
+public class Funcionario extends Usuario {
+    private String cargo;
+
+    public Funcionario(String nome, String cpf, String email, String cargo) {
+        super(nome, cpf, email);
+        this.cargo = cargo;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    @Override
+    public void listarInformacoes() {
+        System.out.println("[FUNCIONÁRIO] Nome: " + getNome() + " | CPF: " + getCpf() + 
+                           " | Email: " + getEmail() + " | Cargo: " + cargo);
+    }
+}
