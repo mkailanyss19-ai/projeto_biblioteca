051e1b5b1781a4fe38d85dad00615cc88b2861f0
mkailanyss19-ai <mkailanyss19@gmail.com>
2026-08-29 21:17:00 -0300

    Update Livro.java

--- biblioteca/Livro.java
+++ biblioteca/Livro.java
@@ -1,1 +1,61 @@
 Livro.java
+
+  package biblioteca;
+
+public class Livro implements Disponibilidade {
+    private String titulo;
+    private String autor;
+    private int anoPublicacao;
+    private boolean disponivel;
+
+    public Livro(String titulo, String autor, int anoPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+        this.disponivel = true;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    @Override
+    public void emprestar() {
+        this.disponivel = false;
+    }
+
+    @Override
+    public void devolver() {
+        this.disponivel = true;
+    }
+
+    @Override
+    public String toString() {
+        String status = disponivel ? "Disponível" : "Emprestado";
+        return "Título: " + titulo + " | Autor: " + autor + " | Ano: " + anoPublicacao + " | Status: " + status;
+    }
+}
